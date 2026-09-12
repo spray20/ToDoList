a713e228548367c30af6d638e5479ad15093404c
Dani <danifahed2011@gmail.com>
2026-09-12 20:47:47 +0300

    бета 1 версия

--- ToDoList.py
+++ ToDoList.py
@@ -1,73 +1,66 @@
 import tkinter as tk
 
 
-root = tk.Tk()         #Создание окна
+root = tk.Tk()
+
 root.title("To-Do List")
 root.geometry("500x600")
 root.resizable(False, False)
-root.title("To-Do List")
 
 
-title = tk.Label(      #Создание заголовка
+title = tk.Label(
     root,
     text="TO-DO LIST",
-    font=("Arial", 30, "bold", )
+    font=("Arial", 30, "bold")
 )
-title.place(x=135,y=30)
+title.place(x=135, y=30)
 
 
-add_task_text = tk.Label(    #Создание надписи, которая говорит пользователю, что делать
+add_task_text = tk.Label(
     root,
     text="Добавьте задачу!",
-    font=("Arial", 18, "bold", )
+    font=("Arial", 18, "bold")
 )
-add_task_text.place(x=25,y=120)
+add_task_text.place(x=25, y=120)
 
 
-entry = tk.Entry(    #Создание Entry поля
+entry = tk.Entry(
     root,
     font=("Arial", 14),
-     #insertbackground="white",
     bd=0,
     relief="flat"
 )
-entry.place(x=25,y=175)
+entry.place(x=25, y=175)
 
 
-def add_task():  #Функция для удалиния написанного после нажатия кнопки
-    task = entry.get()       
+task_y = 230
+
+
+def add_task():
+    global task_y
+
+    task = entry.get()
     entry.delete(0, tk.END)
 
+    task_label = tk.Label(
+        root,
+        text=task,
+        font=("Arial", 14)
+    )
+    task_label.place(x=25, y=task_y)
 
-add_task_button = tk.Button(        #Создание кнопки "Добавить заметку"
+    task_y += 35
+
+
+add_task_button = tk.Button(
     root,
     text="Добавить заметку",
     font=("Arial", 14),
     relief="flat",
     command=add_task
 )
-add_task_button.place(x=300,y=165)
-
-
-def add_task():
-    task = entry.get()       
-    entry.delete(0, tk.END)
-
-    label = tk.Label(root, text=task)
-    label.pack()
-
-
-    
-
-
-
-
-
-
-
+add_task_button.place(x=300, y=165)
 
 
 root.mainloop()
-
-
 
