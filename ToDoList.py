64eb0d7ce9a27266e3c79eb363c3902602f79502
Dani <danifahed2011@gmail.com>
2026-09-19 21:22:06 +0300

    релиз V1.0

--- ToDoList.py
+++ ToDoList.py
@@ -35,31 +35,69 @@ entry.place(x=25, y=175)
 
 task_y = 230
 
+tasks_list = []
+
+
 
 def add_task():
-    global task_y
+    global task_y, task_label
 
     task = entry.get()
     entry.delete(0, tk.END)
 
-    task_label = tk.Label(
+    if task != "":
+
+     task_label = tk.Label(
         root,
         text=task,
         font=("Arial", 14)
     )
-    task_label.place(x=25, y=task_y)
+     task_label.place(x=25, y=task_y)
 
-    task_y += 35
+     task_y += 35
 
+     task_label.bind("<Button-1>", complete_task)
+     tasks_list.append(task_label)
+
+
+
+
+def complete_task(event):
+    event.widget.config(font=("Arial", 14, "overstrike"), fg = "gray")
+    
 
 add_task_button = tk.Button(
     root,
     text="Добавить заметку",
-    font=("Arial", 14),
+    font=("Arial", 14, "bold"),
     relief="flat",
     command=add_task
 )
-add_task_button.place(x=300, y=165)
+
+
+add_task_button.place(x=285, y=165)
+
+def delete_task():
+    global task_y
+
+    for label in tasks_list:
+        label.destroy()
+        
+    tasks_list.clear()
+    task_y = 230  
+   
+
+delate_task_button = tk.Button(
+    root,
+    text="Удалить",
+    font=("Arial", 14, "bold"),
+    relief="flat",
+    command=delete_task
+)
+delate_task_button.place(x=350,y=500)
+
+
+
 
 
 root.mainloop()
